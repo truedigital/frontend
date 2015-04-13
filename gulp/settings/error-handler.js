f48ef96788fe83ca5cabe4c64f66e2115762f7d9
Craig Coles <craigcoles8@gmail.com>
2015-04-13 20:14:36 +0100

    Fix error handling

--- gulp/settings/error-handler.js
+++ gulp/settings/error-handler.js
@@ -1,23 +1,26 @@
 // === ERROR HANDLING
 // ============================================================================
 
+var gutil = require('gulp-util'),
+    browserSync = require('browser-sync');
+
+// function formatError( err ) {
+//   console.log(err);
+//   var errorString = '[' + err.plugin + ']';
+//   errorString += ' ' + err.message.plugins.replace("\n",'');
+//   if(err.fileName)
+//       errorString += ' in ' + err.fileName;
+//   if(err.lineNumber)
+//       errorString += ' on line ' + err.lineNumber;
+//   return errorString;
+// }
+
 module.exports =
 {
-    formatError: function( )
+    handleError: function( err )
     {
-        console.log(err);
-        var errorString = '[' + err.plugin + ']';
-        errorString += ' ' + err.message.plugins.replace("\n",'');
-        if(err.fileName)
-            errorString += ' in ' + err.fileName;
-        if(err.lineNumber)
-            errorString += ' on line ' + err.lineNumber;
-        return errorString;
-    },
-    handleError: function( )
-    {
-        gutil.log(gutil.colors.green(err));
-        browserSync.notify(formatError(err), 360000);
+        gutil.log( gutil.colors.green(err) );
+        browserSync.notify( err , 360000);
         this.emit( 'end' );
     }
 };
