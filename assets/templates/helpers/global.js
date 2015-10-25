050470c61bd347baf977bfd5bd33c49699a85020
Craig Coles <craigcoles8@gmail.com>
2015-10-25 15:21:06 +0000

    Adding more data to the layout

--- assets/templates/helpers/global.js
+++ assets/templates/helpers/global.js
@@ -28,4 +28,11 @@ module.exports.register = function (handlebars) {
 
     });
 
+    handlebars.registerHelper('getYear', function() {
+
+      var date = new Date();
+      return date.getFullYear();
+
+    });
+
 };
